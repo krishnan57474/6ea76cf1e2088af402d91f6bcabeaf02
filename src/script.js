d46f08e5653c7561095e57aeff8b69a125b511ab
Krishnan <krishnan57474@gmail.com>
2020-07-16 12:22:10 +0530

    Added dial

--- src/script.js
+++ src/script.js
@@ -1,10 +1,49 @@
 (function () {
     "use strict";
 
+    function messageAction(data) {
+        switch (data.action) {
+            case "dial": {
+                if (!data.number || !(/[^0-9]/).test(data.number)) {
+                    break;
+                }
+
+                cordova.InAppBrowser.open(
+                    "tel:" + data.number,
+                    "_system"
+                );
+
+                break;
+            }
+        }
+    }
+
+    var Message = (function () {
+        function Message(iawin) {
+            var obj = this;
+
+            iawin.addEventListener("message", function (e) {
+                if (e.type !== "message" || !e.data || !e.data.action) {
+                    return;
+                }
+
+                obj.action(e.data);
+            });
+        }
+
+        Message.prototype = {
+            action: messageAction
+        };
+
+        return Message;
+    }());
+
     function init() {
         var url = atob("aHR0cHM6Ly9oYXl1c3MuY29tLw=="),
-        iawin = window.open(url, "_blank", "location=no,clearcache=yes,zoom=no,footer=no"),
+        iawin = cordova.InAppBrowser.open(url, "_blank", "location=no,clearcache=yes,zoom=no,footer=no"),
         error = false;
+
+        new Message(iawin);
 
         iawin.addEventListener("loaderror", function () {
             error = true;
